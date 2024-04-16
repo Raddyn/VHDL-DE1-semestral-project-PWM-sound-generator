e53179b31c9193cb0a9e2d9b545ea3d69044f0d4
Raddyn <124372068+Raddyn@users.noreply.github.com>
2024-04-16 16:02:18 +0200

    Added enable condition

--- Design sources/duty.vhd
+++ Design sources/duty.vhd
@@ -1,89 +1,87 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
-
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 entity duty is
-    port(
-        clear : in    std_logic;                    --! Clear the display
-        en : in   std_logic;                    --! Enable the display
---        clk : in      std_logic;                    --! Clock signal
---        freq_bin : out    std_logic_vector(14 downto 0);  --! duty of the display
-        pos_duty : out     std_logic_vector(1 downto 0);  --! Current working position
-        left : in     std_logic;                    --! Move to the left
-        right : in    std_logic;                    --! Move to the right
-        increment : in std_logic;                    --! Increment the duty
-        decrement : in std_logic;                    --! Decrement the duty
-        out_1 : out std_logic_vector(3 downto 0);  --! Output for the next stage
-        out_10 : out std_logic_vector(3 downto 0)  --! Output for the next stage
-    );
+  port
+  (
+    clear : in std_logic; --! Clear the display
+    en    : in std_logic; --! Enable the display
+    --        clk : in      std_logic;                    --! Clock signal
+    --        freq_bin : out    std_logic_vector(14 downto 0);  --! duty of the display
+    pos_duty  : out std_logic_vector(1 downto 0); --! Current working position
+    left      : in std_logic; --! Move to the left
+    right     : in std_logic; --! Move to the right
+    increment : in std_logic; --! Increment the duty
+    decrement : in std_logic; --! Decrement the duty
+    out_1     : out std_logic_vector(3 downto 0); --! Output for the next stage
+    out_10    : out std_logic_vector(3 downto 0) --! Output for the next stage
+  );
 
 end entity duty;
 
-
-
 architecture behavioral of duty is
 
-signal int_duty : integer := 50;
-signal int_pos : integer := 1;
+  signal int_duty : integer := 50;
+  signal int_pos  : integer := 1;
 
 begin
 
-    val_change: process(increment, decrement, clear) is   --! Process to change the value of the duty
-    begin
-        if clear = '1' then
-            int_duty <= 50;
-        else
-            if increment = '1' then
-                int_duty <= int_duty + 1 * (10 ** int_pos);
-                if int_duty > 100 then
-                    int_duty <= 100;
-                end if;
-            elsif decrement = '1' then
-                int_duty <= int_duty - 1 * (10 ** int_pos);
-                if int_duty < 0 then
-                    int_duty <= 0;
-                end if;
-            end if;
+  val_change : process (increment, decrement, clear) is --! Process to change the value of the duty
+  begin
+    if clear = '1' then
+      int_duty <= 50;
+    else
+      if increment = '1' then
+        int_duty <= int_duty + 1 * (10 ** int_pos);
+        if int_duty > 100 then
+          int_duty <= 100;
         end if;
-     end process val_change;
-     
-    pos_change: process(left, right, clear) is   --! Process to change the position of the duty
-    begin
-        if clear = '1' then
+      elsif decrement = '1' then
+        int_duty <= int_duty - 1 * (10 ** int_pos);
+        if int_duty < 0 then
+          int_duty <= 0;
+        end if;
+      end if;
+    end if;
+  end process val_change;
+
+  pos_change : process (left, right, clear) is --! Process to change the position of the duty
+  begin
+    if en = '1' then
+      if clear = '1' then
+        int_pos <= 1;
+      else
+        if left = '1' then
+          int_pos <= int_pos + 1;
+          if int_pos > 1 then
             int_pos <= 1;
-        else
-            if left = '1' then
-                int_pos <= int_pos + 1;
-                if int_pos > 1 then
-                    int_pos <= 1;
-                end if;
-            elsif right = '1' then
-                int_pos <= int_pos - 1;
-                if int_pos < 0 then
-                    int_pos <= 0;
-                end if;
-            end if;
+          end if;
+        elsif right = '1' then
+          int_pos <= int_pos - 1;
+          if int_pos < 0 then
+            int_pos <= 0;
+          end if;
         end if;
-    end process pos_change;
+      end if;
+    end if;
+  end process pos_change;
 
-    process (int_pos) is
-    begin
-        case int_pos is
-            when 0 =>
-                pos_duty <= "01";
-            when 1 =>
-                pos_duty <= "10";
-            when others =>
-                pos_duty <= "00";
-        end case;
-    end process;
+  process (int_pos) is
+  begin
+    case int_pos is
+      when 0 =>
+        pos_duty <= "01";
+      when 1 =>
+        pos_duty <= "10";
+      when others =>
+        pos_duty <= "00";
+    end case;
+  end process;
 
---    freq_bin <= std_logic_vector(to_unsigned(int_duty, 15));
+  --    freq_bin <= std_logic_vector(to_unsigned(int_duty, 15));
 
-    out_1 <= std_logic_vector(to_unsigned(int_duty mod 10, 4));
-    out_10 <= std_logic_vector(to_unsigned((int_duty / 10) mod 10, 4));
+  out_1  <= std_logic_vector(to_unsigned(int_duty mod 10, 4));
+  out_10 <= std_logic_vector(to_unsigned((int_duty / 10) mod 10, 4));
 
--- TODO: Make the duty more readable for the next stage, maybe a 4 bit std_logic_vector for every decade
+  -- TODO: Make the duty more readable for the next stage, maybe a 4 bit std_logic_vector for every decade
 end behavioral;
-
--- Design sources/frequency.vhd
+++ Design sources/frequency.vhd
@@ -1,102 +1,98 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
-
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
 entity frequency is
-    port(
-        clear : in    std_logic;                    --! Clear the display
-        en : in   std_logic;                    --! Enable the display
---        clk : in      std_logic;                    --! Clock signal
---        freq_bin : out    std_logic_vector(14 downto 0);  --! Frequency of the display
-        pos : out     std_logic_vector(4 downto 0);  --! Current working position
-        left : in     std_logic;                    --! Move to the left
-        right : in    std_logic;                    --! Move to the right
-        increment : in std_logic;                    --! Increment the frequency
-        decrement : in std_logic;                    --! Decrement the frequency
-        out_1 : out std_logic_vector(3 downto 0);  --! Output for the next stage
-        out_10 : out std_logic_vector(3 downto 0);  --! Output for the next stage
-        out_100 : out std_logic_vector(3 downto 0);  --! Output for the next stage
-        out_1000 : out std_logic_vector(3 downto 0);  --! Output for the next stage
-        out_10000 : out std_logic_vector(3 downto 0)  --! Output for the next stage
-    );
+  port
+  (
+    clear : in std_logic; --! Clear the display
+    en    : in std_logic; --! Enable the display
+    --        clk : in      std_logic;                    --! Clock signal
+    --        freq_bin : out    std_logic_vector(14 downto 0);  --! Frequency of the display
+    pos       : out std_logic_vector(4 downto 0); --! Current working position
+    left      : in std_logic; --! Move to the left
+    right     : in std_logic; --! Move to the right
+    increment : in std_logic; --! Increment the frequency
+    decrement : in std_logic; --! Decrement the frequency
+    out_1     : out std_logic_vector(3 downto 0); --! Output for the next stage
+    out_10    : out std_logic_vector(3 downto 0); --! Output for the next stage
+    out_100   : out std_logic_vector(3 downto 0); --! Output for the next stage
+    out_1000  : out std_logic_vector(3 downto 0); --! Output for the next stage
+    out_10000 : out std_logic_vector(3 downto 0) --! Output for the next stage
+  );
 
 end entity frequency;
 
-
-
 architecture behavioral of frequency is
 
-signal int_freq : integer := 1000;
-signal int_pos : integer := 0;
+  signal int_freq : integer := 1000;
+  signal int_pos  : integer := 0;
 
 begin
 
-    val_change: process(increment, decrement, clear) is   --! Process to change the value of the frequency
-    begin
-        if clear = '1' then
-            int_freq <= 1000;
-        else
-            if increment = '1' then
-                int_freq <= int_freq + 1 * (10 ** int_pos);
-                if int_freq > 20000 then
-                    int_freq <= 20000;
-                end if;
-            elsif decrement = '1' then
-                int_freq <= int_freq - 1 * (10 ** int_pos);
-                if int_freq < 20 then
-                    int_freq <= 20;
-                end if;
-            end if;
+  val_change : process (increment, decrement, clear) is --! Process to change the value of the frequency
+  begin
+    if en = '1' then
+      if clear = '1' then
+        int_freq <= 1000;
+      else
+        if increment = '1' then
+          int_freq <= int_freq + 1 * (10 ** int_pos);
+          if int_freq > 20000 then
+            int_freq <= 20000;
+          end if;
+        elsif decrement = '1' then
+          int_freq <= int_freq - 1 * (10 ** int_pos);
+          if int_freq < 20 then
+            int_freq <= 20;
+          end if;
         end if;
-     end process val_change;
-     
-    pos_change: process(left, right, clear) is   --! Process to change the position of the frequency
-    begin
-        if clear = '1' then
-            int_pos <= 2;
-        else
-            if left = '1' then
-                int_pos <= int_pos + 1;
-                if int_pos > 4 then
-                    int_pos <= 4;
-                end if;
-            elsif right = '1' then
-                int_pos <= int_pos - 1;
-                if int_pos < 0 then
-                    int_pos <= 0;
-                end if;
-            end if;
+      end if;
+    end if;
+  end process val_change;
+
+  pos_change : process (left, right, clear) is --! Process to change the position of the frequency
+  begin
+    if clear = '1' then
+      int_pos <= 2;
+    else
+      if left = '1' then
+        int_pos <= int_pos + 1;
+        if int_pos > 4 then
+          int_pos <= 4;
         end if;
-    end process pos_change;
+      elsif right = '1' then
+        int_pos <= int_pos - 1;
+        if int_pos < 0 then
+          int_pos <= 0;
+        end if;
+      end if;
+    end if;
+  end process pos_change;
 
-    process (int_pos) is
-    begin
-        case int_pos is
-            when 0 =>
-                pos <= "00001";
-            when 1 =>
-                pos <= "00010";
-            when 2 =>
-                pos <= "00100";
-            when 3 =>
-                pos <= "01000";
-            when 4 =>
-                pos <= "10000";
-            when others =>
-                pos <= "00000";
-        end case;
-    end process;
+  process (int_pos) is
+  begin
+    case int_pos is
+      when 0 =>
+        pos <= "00001";
+      when 1 =>
+        pos <= "00010";
+      when 2 =>
+        pos <= "00100";
+      when 3 =>
+        pos <= "01000";
+      when 4 =>
+        pos <= "10000";
+      when others =>
+        pos <= "00000";
+    end case;
+  end process;
 
---    freq_bin <= std_logic_vector(to_unsigned(int_freq, 15));
+  --    freq_bin <= std_logic_vector(to_unsigned(int_freq, 15));
 
-    out_1 <= std_logic_vector(to_unsigned(int_freq mod 10, 4));
-    out_10 <= std_logic_vector(to_unsigned((int_freq / 10) mod 10, 4));
-    out_100 <= std_logic_vector(to_unsigned((int_freq / 100) mod 10, 4));
-    out_1000 <= std_logic_vector(to_unsigned((int_freq / 1000) mod 10, 4));
-    out_10000 <= std_logic_vector(to_unsigned((int_freq / 10000) mod 10, 4));
-
-
--- TODO: Make the frequency more readable for the next stage, maybe a 4 bit std_logic_vector for every decade
+  out_1     <= std_logic_vector(to_unsigned(int_freq mod 10, 4));
+  out_10    <= std_logic_vector(to_unsigned((int_freq / 10) mod 10, 4));
+  out_100   <= std_logic_vector(to_unsigned((int_freq / 100) mod 10, 4));
+  out_1000  <= std_logic_vector(to_unsigned((int_freq / 1000) mod 10, 4));
+  out_10000 <= std_logic_vector(to_unsigned((int_freq / 10000) mod 10, 4));
+  -- TODO: Make the frequency more readable for the next stage, maybe a 4 bit std_logic_vector for every decade
 end behavioral;
-
