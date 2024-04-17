53c226399216f2ecfdfb62772d6a899ae08e7e20
genius-02 <164381067+genius-02@users.noreply.github.com>
2024-04-17 16:25:01 +0200

    x7seg.vhd

--- Design sources/x7seg.vhd
+++ Design sources/x7seg.vhd
@@ -1,129 +1,171 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.04.2024 11:21:10
+-- Design Name: 
+-- Module Name: x7seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 entity x7seg is
-	port(
-    	x: in std_logic_vector(15 downto 0);
-        clk: in std_logic;
-        clr: in std_logic;
-        a_to_g: out std_logic_vector(6 downto 0);
-        an: out std_logic_vector(3 downto 0);
-        dp: out std_logic
-        );
-    end x7seg;
-    
-    
-    
--- 7-segment display driver
-architecture x7seg of x7seg is
-
-signal s: std_logic_vector(1 downto 0);
-signal digit: std_logic_vector(3 downto 0);
-signal aen: std_logic_vector(3 downto 0);
-signal clkdiv: std_logic_vector(20 downto 0);
-
-begin
-	s <= clkdiv(20 downto 19);
-    aen <= "1111";
-    dp <= '1';
-
--- multiplexer
-
-	process(s, x)
-    begin
-    	case s is
-          when "00" => digit <= x(3 downto 0);
-          when "01" => digit <= x(7 downto 4);
-          when "10" => digit <= x(11 downto 8);
-          when others => digit <= x(15 downto 12);
-        end case;
-    end process;
-    
-    
--- bin2seg
-	process(digit)
-    begin
-    	case digit is
-        	when x"0" => a_to_g <= "0000001";		--0
-            when x"1" => a_to_g <= "1001111";		--1
-            when x"2" => a_to_g <= "0010010";		--2
-            when x"3" => a_to_g <= "0000110";		--3
-            when x"4" => a_to_g <= "1001100";		--4
-            when x"5" => a_to_g <= "0100100";		--5
-            when x"6" => a_to_g <= "0100000";		--6
-            when x"7" => a_to_g <= "0001101";		--7
-            when x"8" => a_to_g <= "0000000";		--8
-            when x"9" => a_to_g <= "0000100";		--9
-            when x"A" => a_to_g <= "0001000";		--A
-            when x"B" => a_to_g <= "1100000";		--B
-            when x"C" => a_to_g <= "0110001";		--C
-            when x"D" => a_to_g <= "1000010";		--D
-            when x"E" => a_to_g <= "0110000";		--E
-            when others => a_to_g <= "0111000";		--F
-        end case;
-    end process;
-        	
-
--- Digit select
-
-	process(s, aen)
-    begin
-    	an <= "1111";
-        if aen(conv_integer(s)) = '1' then
-           an(conv_integer(s)) <= '0';
-        end if;
-    end process;
-
-
--- Clock divider
-	
-    process(clk, clr)
-    begin
-    	if clr = '1' then
-           clkdiv <= (others => '0');
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           out_1 : in std_logic_vector(3 downto 0);
+           out_10 : in std_logic_vector(3 downto 0);
+           out_100 : in std_logic_vector(3 downto 0);
+           out_1000 : in std_logic_vector(3 downto 0);
+           out_10000 : in std_logic_vector(3 downto 0);
+           pos_mulx_freq : out std_logic_vector(4  downto 0);
+           seg   : out std_logic_vector(6 downto 0);
            
-        elsif clk'event and clk = '1' then
-           clkdiv <= clkdiv + 1;
-        end if;
-    end process;
+           out_1_duty     : in std_logic_vector(3 downto 0); 
+           out_10_duty    : in std_logic_vector(3 downto 0);
+           pos_mulx_duty : out std_logic_vector(1  downto 0);
+           
+           sw : in std_logic
+           );
 end x7seg;
 
+architecture Behavioral of x7seg is
+    component bin2seg is
+        port(
+            clear : in std_logic;
+            bin : in std_logic_vector(3downto 0);
+            seg : out std_logic_vector(6 downto 0)
+            );
+    end component;
+   
+    component counter is
+        port(
+            clk : in std_logic;
+            rst : in std_logic;
+            en : in std_logic;
+            count: out std_logic_vector(3 downto 0)
+            );
+    end component;
+    
+    component clock_enable is
+    generic(
+        N_PERIODS : integer
+        );
+    port(
+        clk : in STD_LOGIC;
+        rst : in STD_LOGIC;
+        pulse : out STD_LOGIC
+    );
+    end component;
+
+    signal per_mulx: std_logic;
+    signal position: std_logic_vector(3 downto 0);
+    signal seg_data: std_logic_vector(3 downto 0);
+begin
+
+    out2seg : component bin2seg
+        port map(
+            clear => rst,
+            bin => seg_data,
+            seg => seg
+            );
+
+    period : component clock_enable
+        generic map(
+            N_PERIODS => 300000
+            )
+        port map(
+            clk => clk,
+            rst => rst,
+            pulse => per_mulx
+            );
+
+    switch_seg : component counter
+        port map(
+            clk => clk,
+            rst => rst,
+            en => per_mulx,
+            count => position
+            );
+            
 
 
+    process (clk) is
+    begin
+        
+        if (rising_edge(clk)) then
+            if (rst = '1') then
+                seg_data <= "1111111";
+            elsif (sw = '0') then
+                case position is
+                    when "001" =>
+                        seg_data <= out_1;
+                        pos_mulx_freq <= "11110";
+                    
+                    when "010" =>
+                        seg_data <= out_10;
+                        pos_mulx_freq <= "11101";
+                    
+                    when "100" =>
+                        seg_data <= out_100;
+                        pos_mulx_freq <= "11011";
+                    
+                    when "101" =>
+                        seg_data <= out_1000;
+                        pos_mulx_freq <= "10111";
+                    
+                    when "110" =>
+                        seg_data <= out_10000;
+                        pos_mulx_freq <= "01110";
+                    
+                end case;
+                    
+            else
+                case position is
+                    when "001" =>
+                        seg_data <= out_1_duty;
+                        pos_mulx_freq <= "11110";
+                    
+                    when "010" =>
+                        seg_data <= out_10_duty;
+                        pos_mulx_freq <= "11101";
+                    
+                    when "100" =>
+                        seg_data <= "1111111";
+                        pos_mulx_freq <= "11011";
+                    
+                    when "101" =>
+                        seg_data <= "1111111";
+                        pos_mulx_freq <= "10111";
+                    
+                    when "110" =>
+                        seg_data <= "1111111";
+                        pos_mulx_freq <= "01110";
+                end case;
+            end if;
+        end if;
+    end process;
 
 
-
-
-
-
-
---library ieee;
---use ieee.std_logic_1164.all;
---entity x7seg_top is
---	port(
---		mclk: in std_logic;
---		btn: in std_logic_vector(3 downto 3);
---		a_to_g: out std_logic_vector(6 downto 0);
---		an: out std_logic_vector(3 downto 0);
---		dp: out std_logic
---		);
---end x7seg_top;
---
---
---architecture x7seg_top of x7seg_top is
---component x7seg is
---	port(
---    		x: in std_logic_vector(15 downto 0);
---            clk: in std_logic;
---            clr: in std_logic;
---            a_to_g: out std_logic_vector(6 downto 0);
---            an: out std_logic_vector(3 downto 0);
---            dp: out std_logic
---          );
---end component;
---signal x: std_logic_vector(15 downto 0);
---begin
---	x <= x"1234";
---    X1: x7seg port map
---    	(x => x, clk => mclk, clr => btn(3), a_to_g => a_to_g, an =>an, dp => dp);
---end x7seg_top;
+end Behavioral;
