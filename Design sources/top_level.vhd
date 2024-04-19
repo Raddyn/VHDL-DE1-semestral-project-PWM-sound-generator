9fca726b7e2f42a3561049a28699ef1a34cb4c7e
jahuzo <80957105+jahuzo@users.noreply.github.com>
2024-04-19 07:39:33 +0200

    Update top_level.vhd

--- Design sources/top_level.vhd
+++ Design sources/top_level.vhd
@@ -14,9 +14,12 @@ use IEEE.NUMERIC_STD.ALL;
 entity top_level is
  Port (
     SW_MODE : in STD_LOGIC;  -- mode selector switch
-    LED: out STD_LOGIC_VECTOR (15 downto 0)
-
- 
+    LED: out STD_LOGIC_VECTOR (15 downto 0);
+    f_mode_top: in STD_logic;
+    d_mode_top: in std_logic;
+    output: out integer;
+    tclk: in std_logic;
+    btn_rst: in std_logic
   );
 end top_level;
 
@@ -30,12 +33,31 @@ component mode_selector is
            );
    
     end component;
- 
- component top_x7seg is   --tbd, when full integer number => 8 display output functionality  is achieved
     
+ component top_x7seg is   --tbd, when full integer number => 8 display output functionality  is achieved
+    Port (
+        mclk: in std_logic;
+		btn: in std_logic_vector(3 downto 3);
+		a_to_g: out std_logic_vector(6 downto 0);
+		an: out std_logic_vector(3 downto 0);
+		dp: out std_logic
+		);
  
     end component;
+  
+  component clock is 
+    generic (
+        N_PERIODS : integer := 3
+    );
+    port (
+        clk   : in    std_logic; 
+        rst   : in    std_logic; 
+        pulse : out   std_logic  
+    );
     
+    end component;
+  
+  
     signal mode_switch: STD_LOGIC;
     signal rotary_encoder: STD_LOGIC;
     signal audio_out:  STD_LOGIC;
@@ -44,24 +66,39 @@ component mode_selector is
     signal duty_cycle_enable: std_logic;
     signal frequency: integer;
     signal duty_cycle: integer;
-    
+    signal to_disp: integer;
+    signal clk_en: std_logic;
     
 begin
+    clk1: clock
+    generic map (
+            N_PERIODS => 1_000_000
+        )
+        port map (
+            clk   => tclk,
+            rst   => btn_rst,
+            pulse => clk_en
+        );
+
     MS: mode_selector
     port map (
         switch => SW_MODE ,
         frequency_mode => frequency_enable,
         duty_cycle_mode => duty_cycle_enable
+    );
+    DISP: top_x7seg -- fix in top_x7seg
+    port map (
+        mclk => tlck,
+        
     
     );
-    
-    if frequency_mode = '1' then
-        --output <= frequency
-
-    else if duty_cycle_mode = '1' then
-        --output <= duty_cycle
-    
-    end if;
-    
+    process (f_mode_top, d_mode_top)
+    begin
+        if f_mode_top = '1' then
+            output <= frequency;
+        elsif d_mode_top = '1' then
+            output <= duty_cycle;
+        end if;
+    end process;
 
 end Behavioral;
