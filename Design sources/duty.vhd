9c1e39ac0744c5edc63d2845a4f2f003dbea8d40
Raddyn <124372068+Raddyn@users.noreply.github.com>
2024-04-28 21:59:13 +0200

    Update duty.vhd

--- Design sources/duty.vhd
+++ Design sources/duty.vhd
@@ -4,8 +4,8 @@ use IEEE.NUMERIC_STD.all;
 entity duty is
   port
   (
-    clear : in std_logic; --! Clear the display
-    en    : in std_logic; --! Enable the display
+    clear     : in std_logic; --! Clear the display
+    en        : in std_logic; --! Enable the display
     pos_duty  : out std_logic_vector(4 downto 0); --! Current working position
     left      : in std_logic; --! Move to the left
     right     : in std_logic; --! Move to the right
@@ -32,27 +32,39 @@ begin
       if increment = '1' then
         case int_pos is
           when 0 =>
-            int_duty <= int_duty + 1;
+            if int_duty + 1 > 99 then
+              int_duty <= 99;
+            else
+              int_duty <= int_duty + 1;
+            end if;
           when 1 =>
-            int_duty <= int_duty + 10;
+            if int_duty + 10 > 99 then
+              int_duty <= 99;
+            else
+              int_duty <= int_duty + 10;
+            end if;
           when others =>
             int_duty <= int_duty;
         end case;
-        if int_duty > 100 then
-          int_duty <= 100;
-        end if;
       elsif decrement = '1' then
         case int_pos is
           when 0 =>
-            int_duty <= int_duty - 1;
+            if int_duty - 1 < 1 then
+              int_duty <= 1;
+            else
+              int_duty <= int_duty - 1;
+            end if;
           when 1 =>
-            int_duty <= int_duty - 10;
+            if int_duty - 10 < 1 then
+              int_duty <= 1;
+            elsif int_duty = 10 then
+              int_duty <= 10;
+            else
+              int_duty <= int_duty - 10;
+            end if;
           when others =>
             int_duty <= int_duty;
         end case;
-        if int_duty < 0 then
-          int_duty <= 0;
-        end if;
       end if;
     end if;
   end process val_change;
@@ -68,9 +80,9 @@ begin
             int_pos <= 0;
           else
             int_pos <= int_pos + 1;
-          end if; 
+          end if;
         elsif right = '1' then
-          if int_pos - 1 = -1 then
+          if int_pos - 1 =- 1 then
             int_pos <= 1;
           else
             int_pos <= int_pos - 1;
@@ -91,8 +103,6 @@ begin
         pos_duty <= "00000";
     end case;
   end process;
-
-
   out_1  <= std_logic_vector(to_unsigned(int_duty mod 10, 4));
   out_10 <= std_logic_vector(to_unsigned((int_duty / 10) mod 10, 4));
 
