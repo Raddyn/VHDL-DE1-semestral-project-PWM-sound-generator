2dce5bdf5f71382258f96f9bb35215d0fb7df18e
Raddyn <124372068+Raddyn@users.noreply.github.com>
2024-04-28 10:38:10 +0200

    Update frequency.vhd

--- Design sources/frequency.vhd
+++ Design sources/frequency.vhd
@@ -85,12 +85,12 @@ begin
         if left = '1' then
           int_pos <= int_pos + 1;
           if int_pos > 4 then
-            int_pos <= 4;
+            int_pos <= 0;
           end if;
         elsif right = '1' then
           int_pos <= int_pos - 1;
           if int_pos < 0 then
-            int_pos <= 0;
+            int_pos <= 4;
           end if;
         end if;
       end if;
